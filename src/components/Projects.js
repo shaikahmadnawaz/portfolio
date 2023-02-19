502e1fadf46c7b94855156a54c6a04b628aa9cd8
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-02-19 13:17:24 +0530

    projects cards z-index bug fixed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">PROJECTS</p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap -m-4 z-1">
           {projects.map((project) => (
             <a
               href={project.link}
@@ -26,7 +26,7 @@ const Projects = () => {
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-5 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
