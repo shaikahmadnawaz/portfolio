e307c5796103d331062b5f930a89e60a1880fb45
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-11 11:45:22 +0530

    minor bugs fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Contact() {
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 border mx-auto sm:flex-nowrap flex-wrap justify-center">
+      <div className="container px-5 py-10 mx-auto sm:flex-nowrap flex-wrap justify-center">
         <div
           name="contact"
-          className="flex flex-col justify-center align-center md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          className="flex flex-col justify-center items-center md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
@@ -20,12 +20,6 @@ export default function Contact() {
               >
                 <i className="fab fa-github fa-2x"></i>
               </a>
-              <a
-                href="https://www.twitter.com/shaikahmadnawaz"
-                className="text-blue-500 mr-6"
-              >
-                <i className="fab fa-twitter fa-2x"></i>
-              </a>
 
               <a
                 href="https://www.linkedin.com/in/shaik-ahmad-nawaz-894425239/"
@@ -33,6 +27,14 @@ export default function Contact() {
               >
                 <i className="fab fa-linkedin fa-2x"></i>
               </a>
+
+              <a
+                href="https://www.twitter.com/shaikahmadnawaz"
+                className="text-blue-500 mr-6"
+              >
+                <i className="fab fa-twitter fa-2x"></i>
+              </a>
+
               <a
                 href="https://www.instagram.com/shaikahmadnawaz"
                 className="text-blue-500 mr-6"
