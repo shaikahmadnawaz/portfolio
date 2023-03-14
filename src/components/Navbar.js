c6a6c847616f2fddb339b3639cedbd3715b1f0b3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-14 22:06:55 +0530

    smooth scroll added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,52 @@
 import React from "react";
-import { ArrowRightIcon } from "@heroicons/react/solid";
+import { handleScroll } from "../utils/handleScroll";
 
 const Navbar = () => {
   return (
     <header className="bg-black md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Nawaz
-          </a>
+        <a
+          href="#about"
+          onClick={handleScroll}
+          className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl"
+        >
+          Nawaz
         </a>
+
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#about" className="mr-5 hover:text-white">
+          <a
+            href="#about"
+            onClick={handleScroll}
+            className="mr-5 hover:text-white"
+          >
             Home
           </a>
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a
+            href="#projects"
+            onClick={handleScroll}
+            className="mr-5 hover:text-white"
+          >
             Projects
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a
+            href="#skills"
+            onClick={handleScroll}
+            className="mr-5 hover:text-white"
+          >
             Skills
           </a>
           <a
             href="https://shaikahmadnawaz.hashnode.dev/"
             className="mr-5 hover:text-white"
+            onClick={handleScroll}
           >
             Blogs
           </a>
-          <a href="#contact" className="mr-5 hover:text-white">
+          <a
+            href="#contact"
+            onClick={handleScroll}
+            className="mr-5 hover:text-white"
+          >
             Contact
           </a>
         </nav>
