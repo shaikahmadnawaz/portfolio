f34c70460cd6f0772f413eb22d6ad99c1da6105d
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-14 20:48:01 +0530

    Contact Me updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,71 @@
-import React from "react";
+import contactMe from "../utils/contactme";
 
-export default function Contact() {
+const ContactMe = () => {
   return (
-    <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto sm:flex-nowrap flex-wrap justify-center">
-        <div
-          name="contact"
-          className="flex flex-col justify-center items-center md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Contact Me
-          </h2>
-          <p className="leading-relaxed mb-5">Stay Connected</p>
-          <div className="relative mb-4">
-            <div className="flex">
-              <a
-                href="https://www.github.com/shaikahmadnawaz"
-                className="text-blue-500 mr-6"
-              >
-                <i className="fab fa-github fa-2x"></i>
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/shaik-ahmad-nawaz-894425239/"
-                className="text-blue-500 mr-6"
-              >
-                <i className="fab fa-linkedin fa-2x"></i>
-              </a>
-
-              <a
-                href="https://www.twitter.com/shaikahmadnawaz"
-                className="text-blue-500 mr-6"
-              >
-                <i className="fab fa-twitter fa-2x"></i>
-              </a>
-
-              <a
-                href="https://www.instagram.com/shaikahmadnawaz"
-                className="text-blue-500 mr-6"
-              >
-                <i className="fab fa-instagram fa-2x"></i>
-              </a>
-            </div>
-          </div>
-        </div>
+    <div
+      id="contact"
+      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    >
+      <div className="max-w-xl mb-10 md:mx-auto text-center lg:max-w-2xl md:mb-12">
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 -right-28 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="df31b9f6-a505-42f8-af91-d2b7c3218e5c"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#df31b9f6-a505-42f8-af91-d2b7c3218e5c)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative text-white">Contact Me</span>
+          </span>
+        </h2>
       </div>
-    </section>
+      <div className="grid gap-8 row-gap-5 lg:grid-cols-3">
+        {contactMe.map((item) => (
+          <a
+            href={item.link}
+            target="_blank"
+            rel="noreferrer"
+            aria-label=""
+            key={item.title}
+          >
+            <div className="relative  p-px overflow-hidden transition duration-300 transform border rounded shadow-md hover:scale-105 group hover:shadow-xl">
+              <div className="absolute bottom-0 left-0 w-full h-1 duration-300 origin-left transform scale-x-0 bg-white group-hover:scale-x-100" />
+              <div className="absolute bottom-0 left-0 w-1 h-full duration-300 origin-bottom transform scale-y-0 bg-white group-hover:scale-y-100" />
+              <div className="absolute top-0 left-0 w-full h-1 duration-300 origin-right transform scale-x-0 bg-white group-hover:scale-x-100" />
+              <div className="absolute bottom-0 right-0 w-1 h-full duration-300 origin-top transform scale-y-0 bg-white group-hover:scale-y-100" />
+              <div className="relative p-5 bg-gray-800 rounded-sm">
+                <div className="flex flex-col items-center  mb-2 lg:flex-row">
+                  <div className="flex items-center justify-center w-10 h-10 mb-4 mr-2 rounded-full bg-black lg:mb-0">
+                    <i className={`${item.icon}`}></i>
+                  </div>
+                  <h6 className="font-semibold leading-5">{item.title}</h6>
+                </div>
+                <p className="mb-2 text-sm text-white text-center lg:text-start">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
   );
-}
+};
+
+export default ContactMe;
---
+++ src/utils/contactme.js
@@ -0,0 +1,22 @@
+const contactMe = [
+  {
+    title: "Email",
+    description: "Let's get in touch.",
+    link: "mailto:shaikahmadnawaz@gmail.com",
+    icon: "text-yellow-300 text-xl fa-solid fa-envelope",
+  },
+  {
+    title: "LinkedIn",
+    description: "Let's connect.",
+    link: "https://www.linkedin.com/in/shaik-ahmad-nawaz-894425239/",
+    icon: "text-yellow-300 text-xl fa-brands fa-linkedin",
+  },
+  {
+    title: "GitHub",
+    description: "Check out my repos.",
+    link: "https://github.com/shaikahmadnawaz",
+    icon: "text-yellow-300 text-xl fa-brands fa-github",
+  },
+];
+
+export default contactMe;
