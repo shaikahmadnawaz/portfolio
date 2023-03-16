c6a5c1fec88458f15a3767f3cf43239b223d3f09
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-16 09:18:06 +0530

    minor UI fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { handleScroll } from "../utils/handleScroll";
 
 const Home = () => {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="lg:flex-grow md:w-1/2  lg:ml-4 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi there, I'm{" "}
             <span className="inline-block text-yellow-300">Nawaz</span>
@@ -22,13 +23,14 @@ const Home = () => {
             </a>
             <a
               href="#projects"
+              onClick={handleScroll}
               className="ml-4 inline-flex items-center h-14 px-6 py-3 font-medium tracking-wide transition duration-200 shadow-md text-gray-400 bg-gray-800 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
             >
               See My Work
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-lg lg:mr-8 lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
             alt="hero"
