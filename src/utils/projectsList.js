38f48965c7801ab442eaeedf730c82f4bab9809c
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-20 00:27:51 +0530

    projects section updated

--- src/utils/projectsList.js
+++ src/utils/projectsList.js
@@ -1,8 +1,10 @@
 import WorkoutTracker from "../images/workout-tracker.png";
+import CodeEditor from "../images/code-editor.png";
+import WeatherApp from "../images/react-weather-app.png";
 
 const projects = [
   {
-    title: "Workout Tracker",
+    title: "WORKOUT TRACKER",
     description:
       "Developed a workout tracking application using React, Node.js, Express.js, and MongoDB. Implemented authentication and authorization using JSON Web Tokens (JWT), Stored user data securely using MongoDB and encrypted passwords using bcrypt, Allowed users to create, read and delete workout records.",
     image: WorkoutTracker,
@@ -22,44 +24,30 @@ const projects = [
     live: "https://workout-tracker.cyclic.app/signup",
   },
   {
-    title: "Workout Tracker",
+    title: "REAL-TIME CODE EDITOR",
     description:
-      "Developed a workout tracking application using React, Node.js, Express.js, and MongoDB. Implemented authentication and authorization using JSON Web Tokens (JWT), Stored user data securely using MongoDB and encrypted passwords using bcrypt, Allowed users to create, read and delete workout records.",
-    image: WorkoutTracker,
+      "Developed a real-time code editor using React, Node.js, and Express.js. Implemented Web Sockets using socket.io for real-time collaboration, Added feature for creating rooms for multiple users to work on the same code simultaneously. Utilized socket.io for sending and receiving updates in real-time between clients and server.",
+    image: CodeEditor,
     technologies: [
       "React",
-      "Redux",
       "Node.js",
       "Express.js",
-      "MongoDB",
-      "REST API",
-      "JSON Web Tokens (JWT)",
+      "Socket.IO",
       "Git",
       "GitHub",
-      "Cyclic",
+      "Heroku",
     ],
-    github: "https://github.com/shaikahmadnawaz/workout-tracker",
-    live: "https://workout-tracker.cyclic.app/signup",
+    github: "https://github.com/shaikahmadnawaz/realtime-code-editor",
+    live: "https://realtime-code-editor-project.herokuapp.com/",
   },
   {
-    title: "Workout Tracker",
+    title: "REACT WEATHER APP",
     description:
-      "Developed a workout tracking application using React, Node.js, Express.js, and MongoDB. Implemented authentication and authorization using JSON Web Tokens (JWT), Stored user data securely using MongoDB and encrypted passwords using bcrypt, Allowed users to create, read and delete workout records.",
-    image: WorkoutTracker,
-    technologies: [
-      "React",
-      "Redux",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-      "REST API",
-      "JSON Web Tokens (JWT)",
-      "Git",
-      "GitHub",
-      "Cyclic",
-    ],
-    github: "https://github.com/shaikahmadnawaz/workout-tracker",
-    live: "https://workout-tracker.cyclic.app/signup",
+      "Built a weather application using ReactJS and the OpenWeatherMap API. Implemented API calls using Axios to retrieve weather information and update the user interface in real-time.",
+    image: WeatherApp,
+    technologies: ["React", "OpenWeatherMap API", "Git", "GitHub", "Vercel"],
+    github: "https://github.com/shaikahmadnawaz/react-weather-app",
+    live: "https://react-weather-display-application.vercel.app/",
   },
 ];
 
